8e5b4d1264cee67106758c59d273888a4f6d26c2
xtitaniumx <124804927+xtitaniumx@users.noreply.github.com>
2024-02-16 23:16:40 +0700

    Added button components.

---
+++ core/designsystem/src/main/java/com/minux/monitoring/core/designsystem/component/Button.kt
@@ -0,0 +1,110 @@
+package com.minux.monitoring.core.designsystem.component
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.minux.monitoring.core.designsystem.theme.MNXTheme
+import com.minux.monitoring.core.designsystem.theme.grillSansMtFamily
+
+@Composable
+fun MNXRoundedButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    text: String
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        shape = RoundedCornerShape(6.dp),
+        contentPadding = PaddingValues(
+            horizontal = 48.dp
+        )
+    ) {
+        Text(
+            text = text,
+            fontSize = 20.sp,
+            fontFamily = grillSansMtFamily,
+            fontWeight = FontWeight.Normal
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MNXButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    text: String,
+    color: Color = MaterialTheme.colorScheme.primary
+) {
+    CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
+        val shape = RectangleShape
+
+        Button(
+            modifier = modifier
+                .border(
+                    width = 0.5.dp,
+                    color = color,
+                    shape = shape
+                )
+                .padding(3.dp),
+            onClick = onClick,
+            shape = shape,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = color
+            )
+        ) {
+            Text(
+                text = text,
+                fontSize = 20.sp,
+                fontFamily = grillSansMtFamily,
+                fontWeight = FontWeight.Normal
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MNXButtonPreview() {
+    MNXTheme {
+        Column(
+            modifier = Modifier.padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            MNXRoundedButton(
+                onClick = {},
+                text = "Text"
+            )
+            MNXButton(
+                modifier = Modifier.padding(top = 4.dp),
+                onClick = {},
+                text = "Text"
+            )
+            MNXButton(
+                modifier = Modifier.padding(top = 8.dp),
+                onClick = {},
+                text = "Text",
+                color = MaterialTheme.colorScheme.secondary
+            )
+        }
+    }
+}
