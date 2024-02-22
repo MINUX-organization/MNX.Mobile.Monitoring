f1329af1588709fb23c2b06e4e5209cb819667da
xtitaniumx <artemspasenko@gmail.com>
2024-02-22 16:12:08 +0700

    Added new optional parameters to MNXTextField: readOnly, suffix and suffixPadding. The onValueChanged parameter is now called onValueChange and is optional.

--- core/designsystem/src/main/java/com/minux/monitoring/core/designsystem/component/TextField.kt
+++ core/designsystem/src/main/java/com/minux/monitoring/core/designsystem/component/TextField.kt
@@ -3,7 +3,6 @@ package com.minux.monitoring.core.designsystem.component
 import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -12,53 +11,42 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.minux.monitoring.core.designsystem.theme.MNXTheme
+import com.minux.monitoring.core.designsystem.theme.grillSansMtFamily
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MNXTextField(
     modifier: Modifier = Modifier,
     value: String,
-    onValueChanged: (String) -> Unit,
-    hintText: String = ""
+    onValueChange: (String) -> Unit = {},
+    readOnly: Boolean = false,
+    hintText: String = "",
+    suffix: @Composable () -> Unit = {},
+    suffixPadding: Dp = 10.dp
 ) {
-    /*OutlinedTextField(
-        modifier = modifier,
-        value = value,
-        onValueChange = onValueChange,
-        colors = OutlinedTextFieldDefaults.colors(
-            focusedTextColor = Color.White,
-            unfocusedTextColor = MaterialTheme.colorScheme.onPrimaryContainer,
-            focusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
-            unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
-            focusedBorderColor = MaterialTheme.colorScheme.primary,
-            unfocusedBorderColor = MaterialTheme.colorScheme.primary
-        ),
-        shape = RoundedCornerShape(6.dp),
-        singleLine = true,
-        placeholder = {
-            Text(text = "Login")
-        }
-    )*/
-
     val interactionSource = remember {
         MutableInteractionSource()
     }
+
     val shape = RoundedCornerShape(4.dp)
+
     val textStyle = TextStyle(
         color = MaterialTheme.colorScheme.onPrimaryContainer,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+        fontFamily = grillSansMtFamily,
+        fontWeight = FontWeight.Normal
     )
 
     BasicTextField(
@@ -69,7 +57,8 @@ fun MNXTextField(
                 shape = shape
             ),
         value = value,
-        onValueChange = onValueChanged,
+        readOnly = readOnly,
+        onValueChange = onValueChange,
         interactionSource = interactionSource,
         textStyle = textStyle,
         decorationBox = { innerTextField ->
@@ -91,7 +80,7 @@ fun MNXTextField(
                 contentPadding = PaddingValues(
                     start = 10.dp,
                     top = 7.dp,
-                    end = 10.dp,
+                    end = suffixPadding,
                     bottom = 7.dp
                 ),
                 placeholder = {
@@ -99,57 +88,25 @@ fun MNXTextField(
                         text = hintText,
                         style = textStyle
                     )
-                }
+                },
+                suffix = suffix
             )
-
-            /*Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = MaterialTheme.colorScheme.primaryContainer,
-                        shape = shape
-                    )
-                    .border(
-                        width = 1.5.dp,
-                        color = MaterialTheme.colorScheme.primary,
-                        shape = shape
-                    )
-                    .padding(
-                        start = 8.dp,
-                        top = 6.dp,
-                        end = 8.dp,
-                        bottom = 6.dp
-                    )
-            ) {
-                if (value.isEmpty()) {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = hintText,
-                        fontSize = fontSize,
-                        color = MaterialTheme.colorScheme.onPrimaryContainer
-                    )
-                }
-                innerTextField()
-            }*/
         }
     )
-
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun MNXTextFieldPreview() {
     MNXTheme {
-        var text by remember {
+        val text = remember {
             mutableStateOf("")
         }
 
         MNXTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(4.dp),
-            value = text,
-            onValueChanged = { text = it },
+            modifier = Modifier.padding(10.dp),
+            value = text.value,
+            onValueChange = { text.value = it },
             hintText = "Login"
         )
     }
