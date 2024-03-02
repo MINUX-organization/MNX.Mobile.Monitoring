22dcd21ff015017211e38761cecd7ef5d55c60c9
xtitaniumx <artemspasenko@gmail.com>
2024-03-02 22:24:12 +0700

    Transferred all icons from feature modules to :core:designsystem module. Created MNXIcons file containing references to all icons.

---
+++ core/designsystem/src/main/java/com/minux/monitoring/core/designsystem/icon/MNXIcons.kt
@@ -0,0 +1,10 @@
+package com.minux.monitoring.core.designsystem.icon
+
+import com.minux.monitoring.core.designsystem.R.drawable
+
+object MNXIcons {
+    val DropDown = drawable.ic_drop_down
+    val Logo = drawable.ic_logo
+    val Minux = drawable.ic_minux
+    val Wifi = drawable.ic_wifi
+}
