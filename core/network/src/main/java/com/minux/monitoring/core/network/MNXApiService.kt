822f78966db3e11f4fbfcb2bd333be7515a99f97
xtitaniumx <124804927+xtitaniumx@users.noreply.github.com>
2024-03-08 00:39:19 +0700

    Added DTOs and requests for Miner, Mining, Preset, Wallet. In some requests, parameter names were changed: prefixes with entity names were added.

--- core/network/src/main/java/com/minux/monitoring/core/network/MNXApiService.kt
+++ core/network/src/main/java/com/minux/monitoring/core/network/MNXApiService.kt
@@ -5,6 +5,11 @@ import com.minux.monitoring.core.network.model.management.flightsheet.FlightShee
 import com.minux.monitoring.core.network.model.management.flightsheet.FlightSheetInputDto
 import com.minux.monitoring.core.network.model.management.pool.PoolDto
 import com.minux.monitoring.core.network.model.management.pool.PoolInputDto
+import com.minux.monitoring.core.network.model.management.preset.PresetDto
+import com.minux.monitoring.core.network.model.management.preset.PresetInputDto
+import com.minux.monitoring.core.network.model.management.preset.PresetSaveDto
+import com.minux.monitoring.core.network.model.management.wallet.WalletDto
+import com.minux.monitoring.core.network.model.management.wallet.WalletInputDto
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -12,6 +17,7 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MNXApiService {
     @GET("/algorithm/available")
@@ -24,7 +30,7 @@ interface MNXApiService {
     fun addCryptocurrency(@Body cryptocurrencyDto: CryptocurrencyDto): Flow<Result<List<CryptocurrencyDto>>>
 
     @DELETE("/cryptocurrency/{fullName}")
-    fun removeCryptocurrency(@Path("fullName") fullName: String): Flow<Result<Unit>>
+    fun removeCryptocurrency(@Path("fullName") cryptocurrencyFullName: String): Flow<Result<Unit>>
 
     @GET("/flightSheet")
     fun getAllFlightSheets(): Flow<Result<List<FlightSheetDto>>>
@@ -33,13 +39,28 @@ interface MNXApiService {
     fun addFlightSheet(@Body flightSheetInputDto: FlightSheetInputDto): Flow<Result<Unit>>
 
     @POST("/flightSheet/{id}/apply")
-    fun applyFlightSheetToGpus(@Path("id") flightSheetId: String, @Body list: List<String>): Flow<Result<Unit>>
+    fun applyFlightSheetToGpus(@Path("id") flightSheetId: String, @Body gpuIds: List<String>): Flow<Result<Unit>>
 
     @PUT("/flightSheet/{id}")
     fun changeFlightSheet(@Path("id") flightSheetId: String, @Body flightSheetInputDto: FlightSheetInputDto): Flow<Result<Unit>>
 
     @DELETE("/flightSheet/{id}")
     fun removeFlightSheet(@Path("id") flightSheetId: String): Flow<Result<Unit>>
+
+    @GET("/miner/available")
+    fun getAvailableMiners(): Flow<Result<List<String>>>
+
+    @POST("/mining/run")
+    fun startMining(@Body rigIds: List<String>): Flow<Result<Unit>>
+
+    @POST("/mining/stop")
+    fun stopMining(@Body rigIds: List<String>): Flow<Result<Unit>>
+
+    @POST("/mining/powerOff")
+    fun powerOffRig(@Body rigIds: List<String>): Flow<Result<Unit>>
+
+    @POST("/mining/reboot")
+    fun rebootRig(@Body rigIds: List<String>): Flow<Result<Unit>>
 
     @GET("/pool")
     fun getAllPools(): Flow<Result<List<PoolDto>>>
@@ -52,4 +73,31 @@ interface MNXApiService {
 
     @DELETE("/pool/{id}")
     fun removePool(@Path("id") poolId: String): Flow<Result<Unit>>
+
+    @GET("/preset")
+    fun getAllPresets(@Query("gpuName") gpuName: String): Flow<Result<List<PresetDto>>>
+
+    @POST("/preset")
+    fun savePreset(@Body presetSaveDto: PresetSaveDto): Flow<Result<String>>
+
+    @POST("/preset/{id}/apply")
+    fun applyPresetToGpus(@Path("id") presetId: String, @Body gpuIds: List<String>): Flow<Result<Unit>>
+
+    @PUT("/preset/{id}")
+    fun changePreset(@Path("id") presetId: String, @Body presetInputDto: PresetInputDto): Flow<Result<Unit>>
+
+    @DELETE("/preset/{id}")
+    fun removePreset(@Path("id") presetId: String): Flow<Result<Unit>>
+
+    @GET("/wallet")
+    fun getAllWallets(): Flow<Result<List<WalletDto>>>
+
+    @POST("/wallet")
+    fun addWallet(@Body walletInputDto: WalletInputDto): Flow<Result<String>>
+
+    @PUT("/wallet/{id}")
+    fun changeWallet(@Path("id") walletId: String, @Body walletInputDto: WalletInputDto): Flow<Result<Unit>>
+
+    @DELETE("/wallet/{id}")
+    fun removeWallet(@Path("id") walletId: String): Flow<Result<Unit>>
 }
---
+++ core/network/src/main/java/com/minux/monitoring/core/network/model/management/wallet/WalletInputDto.kt
@@ -0,0 +1,7 @@
+package com.minux.monitoring.core.network.model.management.wallet
+
+data class WalletInputDto(
+    val name: String,
+    val address: String,
+    val cryptocurrencyId: String
+)
