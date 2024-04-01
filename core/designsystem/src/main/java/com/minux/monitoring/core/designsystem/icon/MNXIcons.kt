68696a15832862c26b9b64fac610e38b824f45bb
xtitaniumx <124804927+xtitaniumx@users.noreply.github.com>
2024-04-01 23:20:36 +0700

    Added Edit and Show icons. Limited the maximum width of the search component to 160 dp, which is inside the CryptoAssetFilters component.

--- core/designsystem/src/main/java/com/minux/monitoring/core/designsystem/icon/MNXIcons.kt
+++ core/designsystem/src/main/java/com/minux/monitoring/core/designsystem/icon/MNXIcons.kt
@@ -5,6 +5,7 @@ import com.minux.monitoring.core.designsystem.R.drawable
 
 object MNXIcons {
     @DrawableRes val DropDown = drawable.ic_drop_down
+    @DrawableRes val Edit = drawable.ic_edit
     @DrawableRes val Filters = drawable.ic_filters
     @DrawableRes val Logo = drawable.ic_logo
     @DrawableRes val Menu = drawable.ic_menu
@@ -13,5 +14,6 @@ object MNXIcons {
     @DrawableRes val MinuxHeader = drawable.ic_minux_header
     @DrawableRes val MinuxOutline = drawable.ic_minux_outline
     @DrawableRes val Search = drawable.ic_search
+    @DrawableRes val Show = drawable.ic_show
     @DrawableRes val Wifi = drawable.ic_wifi
 }
