10bc843ca43f76f4062d0a7e184317dd99fb3321
xtitaniumx <124804927+xtitaniumx@users.noreply.github.com>
2024-09-29 11:54:25 +0700

    Added :feature:presets:impl module.

---
+++ feature/presets/impl/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.minux.monitoring.android.feature)
+    alias(libs.plugins.minux.monitoring.android.library.compose)
+}
+
+android {
+    namespace = "com.minux.monitoring.feature.presets.impl"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.junit)
+}
