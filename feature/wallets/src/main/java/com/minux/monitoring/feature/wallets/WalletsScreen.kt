9d3dc8a9af988a6afa38f3d8e23c1a3e14f20739
xtitaniumx <artemspasenko@gmail.com>
2024-10-01 09:45:26 +0700

    Replaced removed components with Text, MNXTextField, MNXDropDownMenu and SearchAndSortBar. The changed component has been replaced with NewCryptoAssetCardWithAddButton.

--- feature/wallets/src/main/java/com/minux/monitoring/feature/wallets/WalletsScreen.kt
+++ feature/wallets/src/main/java/com/minux/monitoring/feature/wallets/WalletsScreen.kt
@@ -6,22 +6,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.minux.monitoring.core.data.model.wallet.WalletInputParam
+import com.minux.monitoring.core.designsystem.component.MNXDropDownMenu
+import com.minux.monitoring.core.designsystem.component.MNXTextField
 import com.minux.monitoring.core.designsystem.theme.MNXTheme
-import com.minux.monitoring.core.ui.AddNewCryptoAssetCard
-import com.minux.monitoring.core.ui.CryptoAssetDropDownMenu
-import com.minux.monitoring.core.ui.CryptoAssetFilters
+import com.minux.monitoring.core.designsystem.theme.MNXTypography
+import com.minux.monitoring.core.designsystem.theme.grillSansMtFamily
 import com.minux.monitoring.core.ui.CryptoAssetGrid
-import com.minux.monitoring.core.ui.CryptoAssetTextField
-import com.minux.monitoring.core.ui.CryptoAssetTextFieldTitle
-import com.minux.monitoring.core.ui.CryptoAssetTitle
+import com.minux.monitoring.core.ui.NewCryptoAssetCardWithAddButton
+import com.minux.monitoring.core.ui.SearchAndSortBar
 import com.minux.monitoring.feature.wallets.ui.WalletsStatePreviewParameterProvider
 import com.minux.monitoring.feature.wallets.ui.walletsGridItems
 
@@ -32,9 +36,12 @@ internal fun WalletsScreen(
     onEvent: (WalletsEvent) -> Unit
 ) {
     Column(modifier = modifier) {
-        CryptoAssetTitle(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Wallets"
+        Text(
+            text = "Wallets",
+            style = MNXTypography.headlineMedium.copy(
+                color = MaterialTheme.colorScheme.onPrimary,
+                fontFamily = grillSansMtFamily
+            )
         )
 
         AddNewWalletCard(
@@ -43,11 +50,12 @@ internal fun WalletsScreen(
             onAddWallet = { onEvent(it) }
         )
 
-        WalletsFilters()
+        WalletsFilters(modifier = Modifier.padding(top = 48.dp))
         
         CryptoAssetGrid(
-            headers = listOf("Wallet name", "Coin", "Address", String()),
-            cryptoAssets = walletsState.wallets
+            headers = listOf("Wallet name", "Coin", "Address", ""),
+            cryptoAssetItems = walletsState.wallets,
+            modifier = Modifier.padding(top = 8.dp)
         ) { item, itemPadding ->
             walletsGridItems(
                 item = item,
@@ -59,62 +67,102 @@ internal fun WalletsScreen(
     }
 }
 
+private val walletsTextStyle = TextStyle(
+    fontSize = 16.sp,
+    fontFamily = grillSansMtFamily,
+    fontWeight = FontWeight.Normal,
+    lineHeight = 18.sp
+)
+
 @Composable
 private fun AddNewWalletCard(
     modifier: Modifier = Modifier,
     coins: List<String>,
     onAddWallet: (WalletsEvent.AddWallet) -> Unit
 ) {
-    val walletNameText = remember {
+    val walletName = remember {
         mutableStateOf("")
     }
 
-    val coinText = remember {
+    val coin = remember {
         mutableStateOf(coins.first())
     }
 
-    val cryptoAddressText = remember {
+    val walletAddress = remember {
         mutableStateOf("")
     }
 
-    AddNewCryptoAssetCard(
-        modifier = modifier,
+    NewCryptoAssetCardWithAddButton(
         title = "Add new wallet",
-        onAddButtonClick = {
+        onAddClick = {
             onAddWallet(
                 WalletsEvent.AddWallet(
                     WalletInputParam(
-                        name = walletNameText.value,
-                        address = cryptoAddressText.value,
-                        cryptocurrencyId = coinText.value
+                        name = walletName.value,
+                        address = walletAddress.value,
+                        cryptocurrencyId = coin.value
                     )
                 )
             )
-        }
+        },
+        modifier = modifier
     ) {
-        CryptoAssetTextFieldTitle(text = "Wallet name")
-        CryptoAssetTextField(
-            text = walletNameText,
-            hintText = "My Wallet"
+        Text(
+            text = "Wallet name",
+            color = MaterialTheme.colorScheme.onPrimary,
+            modifier = Modifier.padding(
+                start = 2.dp,
+                top = 12.dp
+            ),
+            style = walletsTextStyle
         )
 
-        CryptoAssetTextFieldTitle(text = "Coin")
-        CryptoAssetDropDownMenu(
-            items = coins,
-            selectedItem = coinText
+        MNXTextField(
+            value = walletName.value,
+            onValueChange = { walletName.value = it },
+            modifier = Modifier.fillMaxWidth(),
+            hint = "My Wallet"
         )
-        
-        CryptoAssetTextFieldTitle(text = "Address")
-        CryptoAssetTextField(
-            text = cryptoAddressText,
-            hintText = "0xawd654654165dfwseawdawdfrs"
+
+        Text(
+            text = "Coin",
+            color = MaterialTheme.colorScheme.onPrimary,
+            modifier = Modifier.padding(
+                start = 2.dp,
+                top = 12.dp
+            ),
+            style = walletsTextStyle
+        )
+
+        MNXDropDownMenu(
+            menuItems = coins,
+            selectedMenuItem = coin.value,
+            onSelectedMenuItemChange = { coin.value = it },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Text(
+            text = "Address",
+            color = MaterialTheme.colorScheme.onPrimary,
+            modifier = Modifier.padding(
+                start = 2.dp,
+                top = 12.dp
+            ),
+            style = walletsTextStyle
+        )
+
+        MNXTextField(
+            value = walletAddress.value,
+            onValueChange = { walletAddress.value = it },
+            modifier = Modifier.fillMaxWidth(),
+            hint = "0xawd654654165dfwseawdawdfrs"
         )
     }
 }
 
 @Composable
-private fun WalletsFilters() {
-    val selectedSortItem = remember {
+private fun WalletsFilters(modifier: Modifier = Modifier) {
+    val selectedSortOption = remember {
         mutableStateOf("Sort by")
     }
 
@@ -122,10 +170,13 @@ private fun WalletsFilters() {
         mutableStateOf("")
     }
 
-    CryptoAssetFilters(
-        sortItems = listOf("Option 1", "Option 2"),
-        selectedSortItem = selectedSortItem,
-        searchText = searchText
+    SearchAndSortBar(
+        sortOptions = listOf("Option 1", "Option 2"),
+        selectedSortOption = selectedSortOption.value,
+        onSelectedSortOptionChange = { selectedSortOption.value = it },
+        searchQuery = searchText.value,
+        onSearchQueryChange = { searchText.value = it },
+        modifier = modifier
     )
 }
 
