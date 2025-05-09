65dbd3495ab7571023059d508b4993a735d14e4b
xtitaniumx <124804927+xtitaniumx@users.noreply.github.com>
2025-05-09 17:31:31 +0700

    Added api module for presets feature.

---
+++ feature/presets/api/src/main/java/com/minux/monitoring/feature/presets/api/PresetsFeatureMediator.kt
@@ -0,0 +1,17 @@
+package com.minux.monitoring.feature.presets.api
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
+
+interface PresetsFeatureMediator {
+
+    @Composable
+    fun AddPresetsFlowScreen(entry: NavBackStackEntry, onShowSnackBar: (String) -> Unit)
+
+    @Composable
+    fun AddPresetConfigurationScreen(
+        entry: NavBackStackEntry,
+        onNavigateUp: () -> Unit,
+        onShowSnackBar: (String) -> Unit
+    )
+}
